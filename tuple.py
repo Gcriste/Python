7161358cb4347f1998f61c9d45a9487665585e28
Griffin <griffincriste@gmail.com>
2020-03-26 13:02:20 -0500

    tuple for loop

--- tuple.py
+++ tuple.py
@@ -1,6 +1,6 @@
 # tuple is a list  but you use paranthesis instead of brackets and you cannot change
 
-months = ('January' , 'February', 'March')
+months = ('January' , 'February', 'March', 'April', 'May', 'June','July', 'August', 'September', 'October', ' November', 'December')
 
 months[1]
 # would print February
