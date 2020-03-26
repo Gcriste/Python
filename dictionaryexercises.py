bf060ef01e8deb22362b6d9f790f6afda7caefd3
Griffin <griffincriste@gmail.com>
2020-03-26 12:39:01 -0500

    dict exercise

--- dictionaryexercises.py
+++ dictionaryexercises.py
@@ -6,3 +6,9 @@ list1 = ["CA", "NJ", "RI"]
 list2 = ["California", "New Jersey", "Rhode Island"]
  
 answer = {list1[i]: list2[i] for i in range(0,3)}
+
+
+list1 = ["CA", "NJ", "RI"]
+list2 = ["California", "New Jersey", "Rhode Island"]
+ 
+dict(zip(list1,list2))  
