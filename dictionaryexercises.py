4dc22b8b6636268deb7d9b8b998f2653b015bafe
Griffin <griffincriste@gmail.com>
2020-03-26 12:39:31 -0500

    another exercise

--- dictionaryexercises.py
+++ dictionaryexercises.py
@@ -12,3 +12,6 @@ list1 = ["CA", "NJ", "RI"]
 list2 = ["California", "New Jersey", "Rhode Island"]
  
 dict(zip(list1,list2))  
+
+person = [["name", "Jared"], ["job", "Musician"], ["city", "Bern"]]
+answer = {thing[0]: thing[1] for thing in person}
