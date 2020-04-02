a2a02b244b3df0235d25e7233c33b750f4c66288
Griffin <griffincriste@gmail.com>
2020-04-02 09:01:31 -0500

    sets

--- sets.py
+++ sets.py
@@ -22,3 +22,28 @@ s.discard({8})
 s.copy()
 # prints
 1,2,3,9,4,5
+
+s.clear()
+#deletes the whole set
+
+#set math
+#math_students | biology_students = union, combines sets, but doesn't make duplicates
+#math_students & biology_students = combines sets, does make duplicates
+
+
+# 1 - Create a variable called numbers which is a tuple with the the values 1, 2, 3 and 4 inside.
+numbers = (1,2,3,4)
+ 
+# 2 - Create a variable called value which is a tuple with the the value 1 inside.
+value = (1,)
+ 
+# 3 - Given the following variable:
+values = [10,20,30]
+# Create a variable called static_values which is the result of the values variable converted to a tuple
+static_values = tuple(values)
+ 
+# 3 - Given the following variable
+stuff = [1,3,1,5,2,5,1,2,5]
+ 
+# Create a variable called unique_stuff which is a set of only the unique values in the stuff list
+unique_stuff = set(stuff)
