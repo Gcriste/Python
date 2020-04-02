3741f0e4d361cdfc703726f4fda650c65cca5319
Griffin <griffincriste@gmail.com>
2020-04-02 08:44:33 -0500

    sets

--- sets.py
+++ sets.py
@@ -4,3 +4,13 @@ s = set({1,2,3,4,5})
 s = set({1,2,3,4,5,5,5,5})
 # would print
 1,2,3,4,5
+
+
+s.add({9})
+#would print (always random order)
+1,2,3,9,4,5
+
+
+s.remove({3})
+#would print 
+1,2,9,4,5
