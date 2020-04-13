a3c616e47959b9557e1cb543b9fe6584d513d472
Griffin <griffincriste@gmail.com>
2020-04-13 10:45:35 -0500

    functions

--- functionsquare.py
+++ functionsquare.py
@@ -7,8 +7,11 @@ print(result)
 
 #set a default for power = 2 (if none is provided), but you can ovveride it by specificing the power
 def exponent(num,power=2):
+    """exponent(num, power) raises num to specified power. Power defaults to 2."""
     return num ** power
 
 print(exponent(2,3)) #8
 print(exponent(3,4)) #81
 print(exponent(7)) #49
+
+print(exponent.__doc__)
