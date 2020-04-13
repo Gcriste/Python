a512d1bfa6b9963c9852e6f6b6db0fb81f1bf8cc
Griffin <griffincriste@gmail.com>
2020-04-13 11:00:33 -0500

    function exercises

--- functionexercises.py
+++ functionexercises.py
@@ -87,3 +87,30 @@ def is_palindrome(string):
 
 def frequency(collection, searchTerm):
     return collection.count(searchTerm)
+
+
+#MULTIPLY EVEN NUMBERS
+#In my solution, I start with a variable called total.
+#Since we're working with multiplication, I start it off as 1.  
+#Then I iterate through the list, checking if each num is cleanly divisible by 2
+#If it is, I multiply total by the number
+#At the end, after the loop finishes, I return total
+def multiply_even_numbers(lst):
+    total = 1
+    for val in lst:
+        if val % 2 == 0:
+            total = total * val
+    return total
+
+
+
+#CAPITALIZE
+#My solution works by:
+
+#Slicing the first character (up to index 1) and capitalizing it
+#Adding that to the rest of the string (from index 1 onward)
+def capitalize(string):
+    return string[:1].upper() + string[1:]
+
+
+    
