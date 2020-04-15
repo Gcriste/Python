73cf92c51ed20edc70a56f0f364b5d023ca2c265
Griffin <griffincriste@gmail.com>
2020-04-15 13:50:25 -0500

    more kwargs

--- functionskwargs.py
+++ functionskwargs.py
@@ -5,3 +5,23 @@ def fav_colors(**kwargs):
 
 
 fav_colors(griffin="blue", hallie="red", tyler="green")
+
+
+def special_greeting(**kwargs):
+    if "david" in kwargs and kwargs["david"] == "special":
+        return "you are the special guest david!"
+    elif "david" in kwargs:
+        return f"{kwargs['david']} david!"
+    return "not sure who this is"
+
+print(special_greeting(david="hello"))
+print(special_greeting(Bob="hello"))
+print(special_greeting(david="special"))
+
+
+def combine_words(word,**kwargs):
+    if 'prefix' in kwargs:
+        return kwargs['prefix'] + word
+    elif 'suffix' in kwargs:
+        return word + kwargs['suffix']
+    return word
