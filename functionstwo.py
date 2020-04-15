3a7b32b162474f9dfa095a3d25ac19ea9285acfa
Griffin <griffincriste@gmail.com>
2020-04-15 13:28:08 -0500

    star args

---
+++ functionstwo.py
@@ -0,0 +1,14 @@
+def sum_all_nums(num1, num2, num3):
+    return num1+num2+num3
+
+print(sum_all_nums(4,6,9))
+
+
+def sum_nums(*args):
+    total = 0
+    for num in args:
+        total+= num
+    return total
+
+print(sum_nums(3,4,51,0))
+print(sum_nums(4,-3,5,6))
